62fa1f99bdbc2f59bc765a56297ae75c049aa5c3
Paweł Marut <pmarut@interia.pl>
2017-04-17 23:03:44 +0200

    Add

--- src/main/java/pma/main/Application.java
+++ src/main/java/pma/main/Application.java
@@ -2,15 +2,40 @@ package pma.main;
 
 
 import java.io.*;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Application {
 
+    private static final Map<String, Integer> commands = new HashMap<>();
+
+    static {
+        commands.put("C", 2);
+        commands.put("M", 3);
+        commands.put("S", 2);
+        commands.put("U", 0);
+        commands.put("R", 0);
+        commands.put("p", 0);
+    }
+
     public static void main(String[] args) {
+
         try {
             Reader reader = new InputStreamReader(System.in, "UTF-8");
             BufferedReader in = new BufferedReader(reader);
 
-            String command = in.readLine();
+            String[] input = in.readLine().split("\\s");
+            String command = input[0];
+
+            if (command != null && !command.trim().isEmpty() && commands.containsKey(command)) {
+                int syntax = commands.get(command);
+                if (syntax == (input.length - 1)) {
+
+                }
+            } else {
+                System.out.println("No command: " + command);
+            }
 
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
